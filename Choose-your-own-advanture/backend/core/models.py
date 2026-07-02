9b661ffa6e1c8a1cab136bba9f68d55c5f776f70
Senthamil selvan J <162943700+Senthamil735826@users.noreply.github.com>
2026-07-02 22:57:57 +0530

    Add files via upload

---
+++ Choose-your-own-advanture/backend/core/models.py
@@ -0,0 +1,18 @@
+from  typing import list, Dict, Any, Optional
+from pydantic import BaseModel, Field
+
+
+class StoryOptionalLLM(BaseModel):
+    text:str = Field(..., description="The text of the option shown to the user")
+    nextNode: Optional[Dict[str, Any]] = Field(None, description="The next node that this option leads to")
+
+
+class StoryNodeLLM(BaseModel):
+    content: str = Field(description="The main content of the story node")
+    isEnding: bool = Field(description="Whether this node is an ending node")
+    isWinningEnding: bool = Field(description="whether this node is a winning ending node")
+    options: Optional[list[StoryOptionalLLM]] = Field(None, description="The options available at this node")
+
+class StoryLLMResponse(BaseModel):
+    title: str = Field(description="The title of the story")
+    rootNode: StoryNodeLLM = Field(description="The root node of the story")
